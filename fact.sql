8df6f97cdd33bcc02daab9d1d86a3bbbda1d5379
Vidhya S <147298749+Vidhya-Soloman@users.noreply.github.com>
2024-03-11 15:12:35 +0530

    Add files via upload

--- fact.sql
+++ fact.sql
@@ -3,3 +3,12 @@ call fact(6);
 
 select * from student;
 call student(); #CALLING THE STUDENT FUNCTION(STUDENT TABLE)
+
+#oddeven procedure
+call oddeven(5);
+
+
+#number procedure
+call number(-2);
+call number(3);
+call number(0);
